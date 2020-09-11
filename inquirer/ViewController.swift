0073fa308f701b74e7ba63c0e7ba03c9608e4105
Nikita <57098717+97nik@users.noreply.github.com>
2020-09-11 09:25:53 +0300

    Initial Commit

---
+++ inquirer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  inquirer
+//
+//  Created by Никита Микрюков on 11.09.2020.
+//  Copyright © 2020 Никита Микрюков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
