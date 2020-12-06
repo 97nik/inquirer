b893335b854cf160a639df439cf002af217079ae
Nikita <97m.nik@gmail.com>
2020-12-06 20:37:14 +0300

    Done     // 1. Передать сюда массив с выбранными ответами     // 2. Определить то животное, которое встречается чаще всего     // 3. Отобразить результаты     // 4. Подумать над логикой определени индекса в соответсвии с диапазоном

--- inquirer/ViewControlles/QuestionsViewController.swift
+++ inquirer/ViewControlles/QuestionsViewController.swift
@@ -22,11 +22,15 @@ class QuestionsViewController: UIViewController {
     @IBOutlet var multipleSwitches: [UISwitch]!
     @IBOutlet var rangedLabels: [UILabel]!
     
-    @IBOutlet weak var rangedSlider: UISlider!
+    @IBOutlet weak var rangedSlider: UISlider!{
+    didSet {
+        let answersCount = Float(questions[questionIndex].answers.count - 1)
+        rangedSlider.maximumValue = answersCount
+    }
+}
     @IBOutlet weak var questionProgressView: UIProgressView!
     
-      // MARK: - Private Properties
-    
+    // MARK: - Private Properties
     private let questions = Question.getQuestions()
     private var questionIndex = 0
     private var answersChoosen: [Answer] = []
@@ -36,20 +40,20 @@ class QuestionsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         updateUI()
     }
     
     @IBAction func singleButtonAnswerPressed(_ sender: UIButton) {
         guard let currentIndex = singleButtons.firstIndex(of: sender) else {
-                   return
-    }
-
-            let currentAnswer = currentAnswers[currentIndex]
-            answersChoosen.append(currentAnswer)
-            
-            nextQuestion()
+            return
         }
+        
+        let currentAnswer = currentAnswers[currentIndex]
+        answersChoosen.append(currentAnswer)
+        
+        nextQuestion()
+    }
     
     @IBAction func multipleAnswerPressed() {
         for (multipleSwitch, answer) in zip(multipleSwitches, currentAnswers) {
@@ -62,83 +66,96 @@ class QuestionsViewController: UIViewController {
     }
     
     @IBAction func rangedAnswerButtonPressed() {
-        
-        let index = lrintf(rangedSlider.value * Float(currentAnswers.count - 1))
+        let index = lrintf(rangedSlider.value)
         answersChoosen.append(currentAnswers[index])
         nextQuestion()
     }
     
-    /*    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // Convert slider value to index (test)
+    @IBAction func rangedSliderAction() {
+        let index = lrintf(rangedSlider.value)
+        questionLabel.text = String(index)
     }
-    */
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let resultsVC = segue.destination as! ResultsViewController
+        resultsVC.answers = answersChoosen
+    }
 
 }
+
 // MARK: - Private Methodx
 extension QuestionsViewController {
-private func updateUI() {
-    // hide stacks
-    for stackView in [singleStackView, multipleStackView, rangedStackView] {
-        stackView?.isHidden = true
+    private func updateUI() {
+        // hide stacks
+        for stackView in [singleStackView, multipleStackView, rangedStackView] {
+            stackView?.isHidden = true
+        }
+        
+        // get current question
+        let currentQuestion = questions[questionIndex]
+        
+        //set current question for question label
+        questionLabel.text = currentQuestion.text
+        
+        // calculate progress
+        let totalProgress = Float(questionIndex) / Float(questions.count)
+        
+        // set progress
+        questionProgressView.setProgress(totalProgress, animated: true)
+        
+        // set navigation title
+        title = "Вопрос № \(questionIndex + 1) из \(questions.count)"
+        
+        // show stacks corresponding to response type
+        showCurrentAnswers(for: currentQuestion.type)
     }
-    // get current question
-           let currentQuestion = questions[questionIndex]
-    //set current question for question label
-           questionLabel.text = currentQuestion.text
-    // calculate progress
-           let totalProgress = Float(questionIndex) / Float(questions.count)
-           
-           // set progress
-           questionProgressView.setProgress(totalProgress, animated: true)
-           
-           // set navigation title
-           title = "Вопрос № \(questionIndex + 1) из \(questions.count)"
     
- //show stacks corresponding to response type
-     showCurrentAnswers(for: currentQuestion.type)
- }
- 
- private func showCurrentAnswers(for type: ResponseType) {
-     switch type {
-     case .single: showSingleAnswers(with: currentAnswers)
-     case .multiple: showMultipleAnswers(with: currentAnswers)
-     case .ranged: showRangedAnswers(with: currentAnswers)
-     }
- }
- 
+    private func showCurrentAnswers(for type: ResponseType) {
+        switch type {
+        case .single: showSingleAnswers(with: currentAnswers)
+        case .multiple: showMultipleAnswers(with: currentAnswers)
+        case .ranged: showRangedAnswers(with: currentAnswers)
+        }
+    }
+    
+    
+    /// Setup single stack view
+    ///
+    /// - Parameter answers: array with answers
+    ///
+    /// Description of method
     private func showSingleAnswers(with answers: [Answer]) {
-         singleStackView.isHidden = false
-         
-         for (button, answer) in zip(singleButtons, answers) {
-             button.setTitle(answer.text, for: .normal)
-         }
-     }
-     
-     private func showMultipleAnswers(with answers: [Answer]) {
-         multipleStackView.isHidden = false
-         
-         for (label, answer) in zip(multipleLabels, answers) {
-             label.text = answer.text
-         }
-     }
-     
-     private func showRangedAnswers(with answers: [Answer]) {
-         rangedStackView.isHidden = false
-         rangedLabels.first?.text = answers.first?.text
-         rangedLabels.last?.text = answers.last?.text
-     }
-     
-     private func nextQuestion() {
-         questionIndex += 1
-         
-         if questionIndex < questions.count {
-             updateUI()
-         } else {
-             performSegue(withIdentifier: "resultSegue", sender: nil)
-         }
-     }
- }
+        singleStackView.isHidden = false
+        
+        for (button, answer) in zip(singleButtons, answers) {
+            button.setTitle(answer.text, for: .normal)
+        }
+    }
+    
+    private func showMultipleAnswers(with answers: [Answer]) {
+        multipleStackView.isHidden = false
+        
+        for (label, answer) in zip(multipleLabels, answers) {
+            label.text = answer.text
+        }
+    }
+    
+    private func showRangedAnswers(with answers: [Answer]) {
+        rangedStackView.isHidden = false
+        rangedLabels.first?.text = answers.first?.text
+        rangedLabels.last?.text = answers.last?.text
+    }
+    
+    private func nextQuestion() {
+        questionIndex += 1
+        
+        if questionIndex < questions.count {
+            updateUI()
+        } else {
+            performSegue(withIdentifier: "resultSegue", sender: nil)
+        }
+    }
+}
+
--- inquirer/ViewControlles/ResultsViewController.swift
+++ inquirer/ViewControlles/ResultsViewController.swift
@@ -10,21 +10,65 @@ import UIKit
 
 class ResultsViewController: UIViewController {
 
+    // 1. Избавиться от кнопкп возврата
+    // 2. Передать сюда массив с выбранными ответами
+    // 3. Определить то животное, которое встречается чаще всего
+    // 4. Отобразить результаты
+    // 5. Подумать над логикой определени индекса в соответсвии с диапазоном
+    
+
+    
+    @IBOutlet var animalTypeLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    var answers: [Answer]!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        navigationItem.hidesBackButton = true
+        updateResult()
+    }
+}
+
+// MARK: - Private Methods
+extension ResultsViewController {
+    
+    private func updateResult() {
+        
+        var frequencyOfAnimals: [AnimalType: Int] = [:]
+        let animals = answers.map { $0.type }
+        
+       
+        for animal in animals {
+            if let animalTypeCount = frequencyOfAnimals[animal] {
+                frequencyOfAnimals.updateValue(animalTypeCount + 1, forKey: animal)
+            } else {
+                frequencyOfAnimals[animal] = 1
+            }
+        }
+      
+        
+        
+//        for animal in animals {
+//            frequencyOfAnimals[animal] = (frequencyOfAnimals[animal] ?? 0) + 1
+//        }
+//
+        
+//        let sortedFrequencyOfAnimals = frequencyOfAnimals.sorted { $0.value > $1.value }
+//        guard let mostFrequencyAnimal = sortedFrequencyOfAnimals.first?.key else { return }
+     
+        // Решение в одну строку:
+        let mostFrequencyAnimal = Dictionary(grouping: answers, by: { $0.type })
+            .sorted(by: {$0.value.count > $1.value.count})
+            .first?.key
+        
+        updateUI(with: mostFrequencyAnimal)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func updateUI(with animal: AnimalType?) {
+        animalTypeLabel.text = "Вы - \(animal?.rawValue ?? "🐶")!"
+        descriptionLabel.text = animal?.definition ?? ""
     }
-    */
-
 }
+
